62fb04e3172682e038b387b1f36baac1357aff49
Takendra saraswat <aaryan224@gmail.com>
2021-09-02 13:38:24 +0530

    First commit

---
+++ app/src/main/java/com/tks/scantopdf/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.tks.scantopdf
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.tks.scantopdf.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val binding=ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        initiateView(binding)
+
+    }
+
+    private fun initiateView(binding: ActivityMainBinding) {
+
+        binding.fabGallery.setOnClickListener {
+
+
+        }
+    }
+}
